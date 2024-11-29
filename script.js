7a30eaf0b2fa61a9e93e36c2a5a7cc048425ad7c
Dan040971 <dansaffiliatewebsite@gmail.com>
2024-11-28 21:44:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Optional interactivity or analytics integration
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Redirecting to Expedia!');
+    });
+});
